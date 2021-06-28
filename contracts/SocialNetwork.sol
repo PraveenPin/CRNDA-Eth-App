25f068ddbff4e74c7ccd51faed4537eff502c95a
PraveenPin <praveen123pinjala@gmail.com>
2021-06-28 20:04:26 +0530

    Added truffle framework alongside hardhat and added SocialNetwork contract with test and tested on mobile phone

---
+++ contracts/SocialNetwork.sol
@@ -0,0 +1,331 @@
+pragma solidity ^0.8.0;
+
+import './owned.sol';
+import "./StringsLib.sol";
+pragma abicoder v2;
+
+contract SocialNetwork is owned{
+    /*Add code */
+    using strings for *;
+
+    string public networkName; /* State variable -> this value gets stored on block chain*/
+    // we cannot know the length of any map and cannot iterate over it, we can only fetch individually
+    mapping(uint => Post) posts;/* Key Value Store, gets written to block chain itself */
+    mapping(uint => User) users;
+    mapping(address => uint) addressToUid;
+    mapping(uint => mapping(uint => Comment)) comments;
+    mapping(string => uint[]) postsFromTags;
+    mapping(uint256 => bool) helperMap;
+
+    uint public postCount = 0;
+    uint16 public userCount = 0;
+
+    struct User{
+        uint id;
+        address userAddress;
+        string name;        
+        mapping(uint => bool) followerIds;
+        mapping(uint => bool) myPostIds; //not user's id
+        mapping(uint => bool) followingIds;
+        uint followersCount;
+        uint followingCount;
+        uint tipObtained;
+        uint tipDonated;
+    } 
+
+    struct Post{
+        uint pid;
+        string content;
+        string url;
+        uint tipAmount;
+        address payable author;
+        uint authorId;
+        string authorName;
+        uint commentsCount;
+        bytes32 picIpfsHash;
+    }
+
+    struct Comment{
+        uint postId;
+        uint cid;
+        string comment;
+        address author;
+        uint authorId;
+        string authorName;
+    }
+
+    constructor () public {
+        networkName = "ETH - SocialNetwork";
+    }
+
+    //events for creating user, following a user, unfollowing a user
+    event AutoCreateUser(address indexed sender, string message);
+    event FollowOrUnFollowUser(address indexed sender, uint senderId, uint author, string message, uint count);
+
+    function getUserIdFromAddress(address _userAddress) public checkAddress view returns (uint){
+        return addressToUid[_userAddress];
+    }
+
+    function getPostFromPostId(uint _postId) public view returns (Post memory){
+        require(_postId > 0 && _postId <= postCount);
+        return posts[_postId];
+    }
+
+    function autoCreateUser(string memory _uname) public {        
+        require(bytes(_uname).length > 0);
+        userCount++;
+        User storage _user = users[userCount];
+        emit AutoCreateUser(msg.sender, "Creating User");            
+        _user.id = userCount;
+        _user.name = _uname;
+        _user.userAddress = msg.sender;
+        _user.followersCount = 0;
+        _user.followingCount = 0;
+        _user.tipObtained = 0;
+        _user.tipDonated = 0;
+        addressToUid[msg.sender] = userCount;
+        emit AutoCreateUser(msg.sender, "User Created");
+    }
+
+    function getUserInfo() public view returns (uint, string memory, uint, uint, uint, uint){     
+        uint _userId = addressToUid[msg.sender];
+        require(_userId > 0);   
+        User storage _user = users[_userId];
+        return (_user.id, _user.name, _user.followersCount, _user.followingCount, _user.tipObtained, _user.tipDonated);
+    }
+
+    function followAuthor(uint _authorId) public checkAddress{
+        uint senderId = addressToUid[msg.sender];
+        require(senderId != _authorId);
+        uint followingCount = users[senderId].followingCount;
+        uint authorsFollowersCount = users[_authorId].followersCount;
+        users[senderId].followingIds[_authorId] = true;
+        users[_authorId].followerIds[senderId] = true;
+        users[senderId].followingCount = followingCount+1;
+        users[_authorId].followersCount = authorsFollowersCount+1;
+        emit FollowOrUnFollowUser(msg.sender, senderId, _authorId, "Following the author with new count", followingCount+1);
+    }
+
+    function unFollowAuthor(uint _authorId) public checkAddress{
+        uint senderId = addressToUid[msg.sender];
+        require(senderId != _authorId);
+        uint followingCount = users[senderId].followingCount;
+        uint authorsFollowersCount = users[_authorId].followersCount;
+        users[senderId].followingIds[_authorId] = false;
+        users[senderId].followingCount = followingCount-1;
+        users[_authorId].followerIds[senderId] = false;
+        users[_authorId].followersCount = authorsFollowersCount-1;
+        emit FollowOrUnFollowUser(msg.sender, senderId, _authorId, "UnFollowing the author with new count", followingCount -1);
+    }
+
+    event FetchMyPosts(address indexed addr, uint[] myPostIds);
+
+    event FetchMyNetworkIds(address indexed sender, uint[] followingIds, uint[] followerIds);
+
+    function getMyPosts(uint _id) public returns (uint[] memory, Post[] memory){        
+        uint myPostCount = 0;
+        uint counter = 0;
+
+        User storage _user = users[_id];
+        for (uint i = 1; i <= postCount; i++){
+            if(_user.myPostIds[i]){
+                myPostCount++;
+            }
+        }
+        
+        uint[] memory postIds = new uint[](myPostCount);
+        Post[] memory myPosts = new Post[](myPostCount);
+        
+        for (uint i = 1; i <= postCount; i++){
+            if(_user.myPostIds[i]){
+                postIds[counter] = i;
+                myPosts[counter++] = posts[i];
+            }
+        }
+        emit FetchMyPosts(msg.sender,postIds);
+        return (postIds, myPosts);
+    }
+
+    function getAllFollowingIds() public returns (uint[] memory, uint[] memory){
+        uint myId = addressToUid[msg.sender];
+        require(myId > 0 && myId <= userCount);
+        User storage _user = users[myId];
+        uint[] memory followingIds = new uint[](_user.followingCount);
+        uint[] memory followerIds = new uint[](_user.followersCount);
+        uint counter = 0;
+        for (uint i = 1; i <= userCount; i++){
+            if(_user.followingIds[i]){
+                followingIds[counter++] = i;
+            }
+        }
+        counter = 0;
+        for (uint i = 1; i <= userCount; i++){
+            if(_user.followerIds[i]){
+                followerIds[counter++] = i;
+            }
+        }
+        emit FetchMyNetworkIds(msg.sender,followingIds,followerIds);
+        return (followingIds,followerIds);
+    }
+    
+    function getWholeNetworkForAnId(uint myId) public returns (uint[] memory, uint[] memory){
+        require(myId > 0 && myId <= userCount);
+        User storage _user = users[myId];
+        uint[] memory followingIds = new uint[](_user.followingCount);
+        uint[] memory followerIds = new uint[](_user.followersCount);
+        uint counter = 0;
+        for (uint i = 1; i <= userCount; i++){
+            if(_user.followingIds[i]){
+                followingIds[counter++] = i;
+            }
+        }
+        counter = 0;
+        for (uint i = 1; i <= userCount; i++){
+            if(_user.followerIds[i]){
+                followerIds[counter++] = i;
+            }
+        }
+        emit FetchMyNetworkIds(_user.userAddress,followingIds,followerIds);
+        return (followingIds,followerIds);
+    }
+
+    function getUserData(uint _uid) public view returns (uint, string memory, uint, uint, uint, uint){     
+        require(_uid > 0);   
+        User storage _user = users[_uid];
+        return (_user.id, _user.name, _user.followersCount, _user.followingCount, _user.tipObtained, _user.tipDonated);
+    }
+
+    event PostCreated(address indexed id, uint pid, string content, string url, uint tipAmount, address author, uint authorId, string authorName, bytes32 imageHash);
+
+    function createPost(string memory _content, string memory _url, bytes32 _imageHash ) public{ 
+        //require valid content
+        require(bytes(_content).length > 0); //if false it stops execution and gas used will be refunded back to caller, else continues execution
+        
+        /* Here _content is a local var and underscore is just a conventions for local vars*/
+        postCount++;        
+        uint userId = addressToUid[msg.sender];
+        posts[postCount] = Post(postCount, _content, "https://github.com/PraveenPin/Simple-Social-Network", 0, payable(msg.sender), userId, users[userId].name, 0, _imageHash); /* Instantiates Post, adds it into map */        
+        //adding the post id to the user
+        users[userId].myPostIds[postCount] = true;
+        //Trigger event from solidity smart contracts
+        //these posts can be open for subscription by consumers
+        emit PostCreated(msg.sender,postCount, _content, _url, 0, msg.sender, userId, users[userId].name, _imageHash);        
+        createTag(_content, postCount);
+    }
+
+    event PostTipped(uint pid, uint tipAmount, address author);
+
+    function tipAPost(uint _id) public payable {
+        require(_id > 0 && _id <= postCount);
+        Post memory _post = posts[_id];
+        address payable _author = _post.author;
+        //paying the author by sending ether
+        payable(address(_author)).transfer(msg.value);
+        //here the tip should be ether, so we use function meta data
+        _post.tipAmount = _post.tipAmount + msg.value;
+        posts[_id] = _post;
+        
+        //add tip amount to user donation
+        users[addressToUid[msg.sender]].tipDonated += msg.value;
+        //add tip amount to author's obtained tip
+        users[_post.authorId].tipObtained += msg.value;
+        emit PostTipped(postCount, _post.tipAmount, _author);
+    }
+
+    //functions, events to create and delete comments
+    event CommentDeleted(uint pid, uint cid, string deletedComment, address commentAuthor, string authorName);
+    event CommentCreated(uint pid, uint cid, string comment, address commentedUser, string authorName);
+
+    function createComment(uint _pid, string memory _comment) public {
+        require(_pid > 0 && _pid <= postCount);
+        Post memory _post = posts[_pid];
+        uint commentsCount = _post.commentsCount;
+        commentsCount++;
+        User storage _user = users[addressToUid[msg.sender]];
+        comments[_pid][commentsCount] = Comment(_pid, commentsCount, _comment, msg.sender, _user.id, _user.name);
+        posts[_pid].commentsCount = commentsCount;
+        emit CommentCreated(_pid, commentsCount, _comment, msg.sender, _user.name);
+    }
+
+    function fetchAllComments(uint _postId) public view returns (Comment[] memory){
+        require(_postId > 0 && _postId <= postCount);
+        Post memory _post = posts[_postId];
+        uint commentsCount = _post.commentsCount;
+        Comment[] memory postComments = new Comment[](commentsCount);
+        uint counter = 0;
+        for(uint i=1; i<= commentsCount; i++){
+            postComments[counter++] = comments[_postId][i];
+        }
+        return postComments;
+    }
+
+    //event for tag creation
+    event PostTagsCreated(string[] tags, uint postId);
+    event AllPostsOfATag(string tag, Post[] posts);
+    event emitWord(string tag);
+
+    function createTag(string memory _content, uint _postId) public {       
+        
+        strings.slice memory s  = _content.toSlice();
+        strings.slice memory delim = " ".toSlice();
+        string[] memory parts = new string[](s.count(delim) + 1);
+        for(uint i = 0; i < parts.length; i++) {
+            parts[i] = s.split(delim).toString();
+        }
+
+        for(uint i = 0; i<parts.length;i++){
+            string memory tag = parts[i];
+            uint[] storage postIds = postsFromTags[tag];
+            postIds.push(_postId);
+            postsFromTags[tag] = postIds;
+            // bool found = false;
+            // for(uint j=0; j<postIds.length; i++){
+            //     if(!found && postIds[j] == _postId){
+            //         found = true;
+            //         break;
+            //     }
+            // }
+            // if(!found){
+            // }
+        }
+
+
+        emit PostTagsCreated(parts, _postId);
+    }
+
+    function checkEmptyCharacter(bytes1 _char) public pure returns (bool){
+        if(_char == 0x20){
+            return true;
+        }
+        return false;
+    }
+
+    function getPostsFromTag(string memory tag) public returns (Post[] memory){
+        uint[] memory postIds = postsFromTags[tag];
+        uint counter = 0;
+        Post[] memory postsOfTag = new Post[](postIds.length);
+        for(uint i=0;i< postIds.length;i++){
+            if(!helperMap[postIds[i]]){
+                counter++;
+                helperMap[postIds[i]] = true;
+                postsOfTag[i] = posts[postIds[i]];
+            }
+        }
+
+        //nullify helper map
+        for(uint i=0;i< postIds.length;i++){
+            helperMap[postIds[i]] = false;
+        }
+        emit AllPostsOfATag(tag, postsOfTag);
+        return postsOfTag;
+    }
+
+    //self destruct event 
+    event DestroyContract(address sender, address caller, uint balance);
+
+    function destroyContract() public onlyOwner{
+        emit DestroyContract(msg.sender, owner, owner.balance);
+        selfdestruct(payable(msg.sender));
+    }
+
+}
--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,7 +7,7 @@ import localhost from 'react-native-localhost';
 import Web3 from 'web3';
 
 import { expo } from '../app.json';
-import Hello from '../artifacts/contracts/Hello.sol/Hello.json';
+import SocialNetwork from '../abis/SocialNetwork.json';
 
 const styles = StyleSheet.create({
   center: { alignItems: 'center', justifyContent: 'center' },
@@ -17,49 +17,85 @@ const styles = StyleSheet.create({
 
 const shouldDeployContract = async (web3, abi, data, from: string) => {
   // const deployment = new web3.eth.Contract(abi).deploy({ from: data });
-  const deployment = web3.eth.Contract(abi, data);
+  // const deployment = new web3.eth.Contract(abi, data);
       
-  const gas = await deployment.estimateGas();
-  const {
-    options: { address: contractAddress },
-  } = await deployment.send({ from, gas });
-  return new web3.eth.Contract(abi, contractAddress);
+  // const gas = await deployment.estimateGas();
+  // const {
+  //   options: { address: contractAddress },
+  // } = await deployment.send({ from, gas });
+  // return new web3.eth.Contract(abi, contractAddress);
+
+  const networkId = await web3.eth.net.getId();
+  const networkData = SocialNetwork.networks[networkId];  
+  let socialNetwork = null;
+  if(networkData){
+    //bad to override transactionConfirmationBlocks' value, overridden here for test environment
+    socialNetwork = new web3.eth.Contract(SocialNetwork.abi, networkData.address, {transactionConfirmationBlocks: 1});    
+  }
+  return socialNetwork;
 };
 
 function App(): JSX.Element {
   const connector = useWalletConnect();
+  const [account, setAccount] = React.useState("");
+  const [user, setUser] = React.useState(0);
   const [message, setMessage] = React.useState<string>('Loading...');
+  const [userInfo, setUserInfo] = React.useState({id: 0 ,name: "No User"});
+
+
   const web3 = React.useMemo(
-    () => new Web3(new Web3.providers.HttpProvider(`http://${localhost}:${HARDHAT_PORT}`)),
+    () => new Web3(new Web3.providers.HttpProvider(`http://192.168.0.8:7545`)),
     [HARDHAT_PORT]
   );
   React.useEffect(() => {
     (async () => {
-      const { address } = await web3.eth.accounts.privateKeyToAccount(HARDHAT_PRIVATE_KEY);
-      console.log("addres",address);
+      // const { address } = await web3.eth.accounts.privateKeyToAccount(HARDHAT_PRIVATE_KEY);
+      const accounts = await web3.eth.getAccounts();
+      console.log("addres",accounts);
       const contract = await shouldDeployContract(
         web3,
-        Hello.abi,
-        Hello.bytecode,
-        address
+        SocialNetwork.abi,
+        SocialNetwork.bytecode,
+        "0x6f0a752416d88452e919fddc777564a3a931548c"
       );
-      setMessage(await contract.methods.greet().call());
+      setMessage(await contract.methods.networkName().call());
+      setAccount(accounts[0]);
+      const user = await contract.methods.getUserIdFromAddress(accounts[0]).call();
+      console.log("User id", user);
+      if(!user){
+        setUser(web3.utils.hexToNumber(user));
+      }
+      let gasReq = await contract.methods.autoCreateUser("Praveen Pinjala - Deployer").estimateGas({ from : accounts[0] });
+      console.log("res*****",gasReq);
+      const resp = await contract.methods.autoCreateUser("Praveen Pinjala - Deployer").send({from: accounts[0], gas:106975});      
+      const userInfo = await contract.methods.getUserInfo().call({from : accounts[0]});
+      let userObj = {
+        id: userInfo[0],
+        name: userInfo[1],
+        followersCount: userInfo[2],
+        followingCount: userInfo[3],
+        tipObtained: userInfo[4],
+        tipDonated: userInfo[5]
+      };
+      setUserInfo(userObj);     
     })();
-  }, [web3, shouldDeployContract, setMessage, HARDHAT_PRIVATE_KEY]);
+  }, [web3, shouldDeployContract, setMessage, HARDHAT_PRIVATE_KEY, setAccount, setUser, setUserInfo]);
   const connectWallet = React.useCallback(() => {
     return connector.connect();
   }, [connector]);
   const signTransaction = React.useCallback(async () => {
     try {
-       await connector.signTransaction({
+       const resp = await connector.signTransaction({
         data: '0x',
-        from: '0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3',
+        from: '0xdef61906c3c996e25c089c6c025ff12b15103ba6',
         gas: '0x9c40',
         gasPrice: '0x02540be400',
         nonce: '0x0114',
-        to: '0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359',
-        value: '0x00',
+        to: '0x01779AFC624Ff35dEECDd294C5ac072F68Ee68Eb',
+        value: '0x01',
       });
+
+      console.log("Sign trans", resp);
     } catch (e) {
       console.error(e);
     }
@@ -69,7 +105,7 @@ function App(): JSX.Element {
   }, [connector]);
   return (
     <View style={[StyleSheet.absoluteFill, styles.center, styles.white]}>
-      <Text testID="tid-message">{message}</Text>
+      <Text testID="tid-message">{`${message} - ${userInfo.name}`}</Text>
       {!connector.connected && (
         <TouchableOpacity onPress={connectWallet}>
           <Text>Connect a Wallet</Text>
